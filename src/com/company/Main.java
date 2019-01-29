756c04ecb36afd0b26561461afa86b1366d37348
v-yahris <iavorh@gmail.com>
2019-01-29 15:41:53 +0000

    Basic pre-prepped statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,38 @@
 package com.company;
 
+import oracle.sql.ORAData;
+
 import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.sql.*;
+
+import static com.company.DBType.ORADB;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
 	// write your code here
 
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+
+        try{
+            conn = DBUtil.getConnection(ORADB);
+            String sql = "Select * From dba_users where Rownum < ?";
+
+            preparedStatement= conn.prepareStatement
+                    (sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+            preparedStatement.setDouble(1, 10);
+
+            rs = preparedStatement.executeQuery();
+
+            while(rs.next()){System.out.println(rs.getString("username"));}
+
+        }catch (SQLException e){}
+
+
+/*
         try(
         Connection conn = DBUtil.getConnection(DBType.ORADB);
         Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
@@ -21,6 +46,33 @@ public class Main {
             }
         }
         //test
+        */
+/*
+        try(
+                Connection conn = DBUtil.getConnection(DBType.ORADB);
+                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                ResultSet rs = stmt.executeQuery("Select username, password from dba_users");
+        ){
 
+            rs.absolute(7);
+            System.out.println(rs.getString("username"));
+            System.out.println(rs.getString("password"));
+
+            rs.moveToInsertRow();
+            rs.updateString("username", "dick");
+            rs.updateString("password", "dick");
+            rs.insertRow();
+
+            System.out.println("Updating name");
+            rs.updateString("username", "HRDept");
+            System.out.println("Updating name");
+            rs.updateRow();
+
+            System.out.println("Record updated succesfully");
+            System.out.println(rs.getString("username"));
+            System.out.println(rs.getString("password"));
+        }
+        //test
+*/
     }
 }
